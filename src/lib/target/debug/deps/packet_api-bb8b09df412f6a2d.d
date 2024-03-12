bc70126720eff46fce15334f9a8d6ec737db0eff
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2024-03-12 09:07:47 +0000

    remoted lib target

--- src/lib/target/debug/deps/packet_api-bb8b09df412f6a2d.d
+++
@@ -1,8 +0,0 @@
-/prog/rust/minecraft_server/src/lib/target/debug/deps/libpacket_api-bb8b09df412f6a2d.rmeta: lib.rs datatypes.rs datatypes/helpers.rs datatypes/validation.rs
-
-/prog/rust/minecraft_server/src/lib/target/debug/deps/packet_api-bb8b09df412f6a2d.d: lib.rs datatypes.rs datatypes/helpers.rs datatypes/validation.rs
-
-lib.rs:
-datatypes.rs:
-datatypes/helpers.rs:
-datatypes/validation.rs:
