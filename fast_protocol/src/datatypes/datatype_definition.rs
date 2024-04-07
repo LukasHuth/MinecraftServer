31a3587e39ab5b5e410632e1cfd61fed494deaa8
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2024-04-07 23:12:43 +0000

    fix: commented out currently unimplemented type

--- fast_protocol/src/datatypes/datatype_definition.rs
+++ fast_protocol/src/datatypes/datatype_definition.rs
@@ -4,7 +4,7 @@ use tokio::io::AsyncWrite;
 
 pub mod important_enums;
 
-pub struct EntityMetadata(entmet_lib::EntityMetadata);
+pub struct EntityMetadata(/*entmet_lib::EntityMetadata*/);
 impl DataWriter for EntityMetadata {
     async fn write(&self, _writer: &mut (impl AsyncWrite + Unpin)) -> binary_utils::Result<()> {
         todo!()
